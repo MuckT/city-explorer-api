ff54f09c4412d23b5391e23d695e6178bce9f3a6
MuckT <mcgtom10@gmail.com>
2021-04-18 12:09:03 -0700

    feat: external api, error handlers, and refactor server

---
+++ handlers/weather.js
@@ -0,0 +1,25 @@
+const superagent = require('superagent');
+
+const getWeather = async(req, res, next) => {
+  try {
+    const url = 'https://api.weatherbit.io/v2.0/forecast/daily';
+    const query = {
+      lat: req.query.lat,
+      lon: req.query.lon,
+      key: process.env.WEATHER_API_KEY
+    };
+    let weatherResponse = await superagent.get(url).query(query)
+    console.log(weatherResponse)
+    let weather = weatherResponse.body.data.map(dailyWeather => new Forecast(dailyWeather));
+    res.json(weather);
+  } catch(err) {
+    next(err);
+  }
+}
+
+function Forecast(day) {
+  this.date = day.datetime;
+  this.description = `Low of ${day.low_temp}, high of ${day.high_temp} with ${day.weather.description}`;
+}
+
+module.exports = getWeather;
